158b4ed8b40fbf882aee1330d9d5892ea938a4d4
dikhmasir22 <56808112+dikhmasir22@users.noreply.github.com>
2024-03-26 16:32:29 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,13 +5,13 @@ export PYTHONUNBUFFERED=true
 VIRTUALENV=.data/venv
 
 if [ ! -d $VIRTUALENV ]; then
-  python3.12 -m venv $VIRTUALENV
+  python3 -m venv $VIRTUALENV
 fi
 
 if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python3.12
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/pip/3.5/get-pip.py | $VIRTUALENV/bin/python3
 fi
 
 $VIRTUALENV/bin/pip install -r requirements.txt
 
-$VIRTUALENV/bin/python3.12 app.py
+$VIRTUALENV/bin/python3 app.py
